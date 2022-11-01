50c3dc81a240c39e5e9636ca94a44df396789dc7
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-11-01 14:37:46 +0100

    chore: add danger

---
+++ dangerfile.js
@@ -0,0 +1,76 @@
+// Import the feedback functions
+import * as danger from 'danger';
+
+/**
+ * Setup
+ */
+const { pr } = danger.github;
+const { created_files } = danger.git;
+const danger_commits = danger.git.commits;
+
+const RELEASE_TRIGGER_REGEX = /^(feat|fix|perf)[:(]/;
+const BREAKING_CHANGE_REGEX = /BREAKING CHANGE:/g;
+
+/** No ENV files */
+const env_files = created_files.find((file) => file.startsWith('.env'));
+if (env_files) {
+  danger.fail(
+    `🙈 This PR contains \`.env\` files. Please remove files before mergin this PR.`
+  );
+}
+
+/** No Fixups */
+const are_fixups = danger_commits.find((commit) =>
+  commit.message.startsWith('fixup!')
+);
+if (are_fixups) {
+  danger.fail(
+    'This PR contains unsquashed commits. Please use `--autosquash`.'
+  );
+}
+
+/* If it's not a branch PR */
+if (pr.base.repo.full_name !== pr.head.repo.full_name) {
+  danger.warn(
+    "🕵️ This PR comes from a fork, and won't get JS generated for QA testing this PR inside the Emission Example app."
+  );
+}
+
+/** No lock file */
+const packageChanged = danger.git.modified_files.includes('package.json');
+const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
+
+if (packageChanged && !lockfileChanged) {
+  danger.warn(
+    `Changes were made to package.json, but not to yarn.lock - <i>'Perhaps you need to run yarn?'</i>`
+  );
+}
+
+const hasReleaseTriggers = danger.git.commits.reduce(
+  (hasReleaseTriggers, commit) =>
+    hasReleaseTriggers ||
+    RELEASE_TRIGGER_REGEX.test(commit.message) ||
+    BREAKING_CHANGE_REGEX.test(commit.message),
+  false
+);
+
+/** No significatn release */
+if (!hasReleaseTriggers) {
+  danger.warn(
+    'Pull request will not create new release as no significant commit types were found'
+  );
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~ Achievemnts                                                            ~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/**
+ * Rule: Celebrate PRs that remove more code than they add.
+ * Reason: Less is more!
+ */
+if (danger.github.pr.deletions > danger.github.pr.additions) {
+  danger.message(
+    `👏 Great job! I see more lines deleted than added. Thanks for keeping us lean!`
+  );
+}

35abe7115bba2b18d1eb15a859a59ecdf099403b
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-11-04 11:44:16 +0100

    feat: add logger

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { apolloServer } from './server';
 import express from 'express';
+import winston from 'winston';
 
 const server = async () => {
   const app = await express();
@@ -9,9 +10,30 @@ const server = async () => {
 
   const PORT = process.env.PORT || 4000;
 
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.File({
+        filename: './logs/error.log',
+        level: 'error',
+      }),
+      new winston.transports.File({ filename: './logs/combined.log' }),
+    ],
+  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      })
+    );
+  }
+
   app.listen(PORT, () => {
     // eslint-disable-next-line no-console
-    console.log(`🚀 Server ready at http://localhost:${PORT}/`);
+    console.log(`🚀 Game Server ready at http://localhost:${PORT}/graphql`);
   });
 };
 

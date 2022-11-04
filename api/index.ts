df68add3942a27954b9da7b16b19bf0ba9fefb7d
Jaroslaw Grishunin <41283085+oslavdev@users.noreply.github.com>
2022-11-04 11:40:08 +0100

    feat: migrate to express server

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,21 @@
-import { server } from './server'
-server.listen().then(({ url }) => {
+import { apolloServer } from './server';
+import express from 'express';
+
+const server = async () => {
+  const app = await express();
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`🚀 Server ready at http://localhost:${PORT}/`);
+  });
+};
+
+server().catch((err) => {
   // eslint-disable-next-line no-console
-  console.log(`🚀 Server ready at ${url}`)
-})
+  console.error(err);
+});
